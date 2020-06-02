c37ac069b7ea1c545d5ef937b56f3cc03a8d4b19
iamkaleb <kalebdmoran@gmail.com>
2020-06-02 14:29:13 -0500

    Added React Player prototype to Player.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Player from "./components/Player"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Player />
   </React.StrictMode>,
   document.getElementById('root')
 );

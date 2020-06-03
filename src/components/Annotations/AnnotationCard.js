1c32c9171d4345e213397549c893aa99538fb8d0
iamkaleb <kalebdmoran@gmail.com>
2020-06-03 16:39:55 -0500

    Added example annotation element

---
+++ src/components/Annotations/AnnotationCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Annotations from "./Annotations.js"
+
+const AnnotationCard = props => {
+    return (
+        <section>
+            <Annotations />
+        </section>
+    );
+}
+
+export default AnnotationCard

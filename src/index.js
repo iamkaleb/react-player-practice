f6e63c674f3842e396e9382e786282fe851faa47
iamkaleb <kalebdmoran@gmail.com>
2020-06-06 17:55:00 -0500

    added redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import NoteList from "./components/NoteList"
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
   <React.StrictMode>
-    <NoteList />
+    <Provider store={store}>
+      <NoteList />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
